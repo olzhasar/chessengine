8ff027443346a0be394adc53ee132228d3181857
olzhasar <o.arystanov@gmail.com>
2026-07-28 18:40:30 +0500

    Print position

--- src/root.zig
+++ src/root.zig
@@ -1,21 +1,32 @@
 const std = @import("std");
 
-const Color = enum {
+const Color = enum(u1) {
     White,
     Black,
 };
 
-const Piece = enum {
+const Piece = enum(u3) {
     Pawn,
     Knight,
     Bishop,
     Rook,
     Queen,
     King,
+
+    fn symbol(self: Piece) u8 {
+        return switch (self) {
+            Piece.Pawn => 'P',
+            Piece.Knight => 'N',
+            Piece.Bishop => 'B',
+            Piece.Rook => 'R',
+            Piece.Queen => 'Q',
+            Piece.King => 'K',
+        };
+    }
 };
 
 const Position = struct {
-    // [color][piece] bitmasks. Bit 0 - a1, bit 63 is h8.
+    // [color][piece type] bitmasks. LSB (bit 0) - a1, MSB (bit 63) - h8.
     pieces: [2][6]u64,
 
     fn start() Position {
@@ -42,8 +53,34 @@ const Position = struct {
     }
 
     fn print(self: *const Position) void {
-        _ = self;
-        std.debug.print("Position\n", .{});
+        var buf: [8][8]u8 = undefined;
+        for (0..64) |i| {
+            buf[i / 8][i % 8] = 32;
+        }
+
+        inline for (0..2) |ci| {
+            inline for (std.enums.values(Piece), 0..) |piece, pi| {
+                const bitmask = self.pieces[ci][pi];
+
+                for (0..64) |i| {
+                    if ((bitmask >> @truncate(i)) & 1 == 1) {
+                        buf[i / 8][i % 8] = piece.symbol();
+                    }
+                }
+            }
+        }
+
+        std.debug.print("-" ** 33, .{});
+        std.debug.print("\n", .{});
+        for (0..8) |r| {
+            std.debug.print("|", .{});
+            for (0..8) |c| {
+                std.debug.print(" {c} |", .{buf[r][c]});
+            }
+            std.debug.print("\n", .{});
+            std.debug.print("-" ** 33, .{});
+            std.debug.print("\n", .{});
+        }
     }
 };
 
